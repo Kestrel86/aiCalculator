23229aaaa1d4c594fa2813ac27feb8b1b7de1c91
Kestrel86 <90483954+Kestrel86@users.noreply.github.com>
2025-02-06 18:08:50 -0800

    first commit

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+# Build a calculator to add, subtract, multiply, divide numbers
+# Evaluate expression wtih Power (**), Trigonometry, log and exponent functions
+
+# RECOMMENDED: Use prompt engineering and AI API from GPT or Gemini or any to evaluate user expressions
+
+# Provide rough estimate of hours spent
+# 40 hours
+# LOG TIME SPENT
+# Divide time between: Requirements analysis & Design, Development, Testing
+# Note down bugs found, number of bugs found
+# compare total time spent with initial estimate
+# Submission Requirements:
+# Source code, Screenshots, Write-up with estimated vs actual hours spent on the assignment
